527814612696a53b0d655e808cda0205532cf0a8
Joshh64 <joshandrewfrancis@gmail.com>
2023-04-21 17:22:02 +0100

    fixed ANS button and added keyboard functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import { evaluate } from 'mathjs';
 
 const Calculator = () => {
   const [input, setInput] = useState('');
+  const [prevAnswer, setPrevAnswer] = useState('');
 
   const handleClick = (button) => {
+    if (button === '=') {
+      const answer = evaluate(input);
+      setInput(answer);
+      setPrevAnswer(answer);
+    } else if (button === 'C') {
+      setInput('');
+    } else if (button === 'ANS') {
+      setInput(prevInput => prevInput + prevAnswer);
+    } else {
+      setInput(prevInput => prevInput + button);
+    }
+  }
+
+  const handleButtonPress = (button) => {
     if (button === '=') {
       setInput(evaluate(input));
     } else if (button === 'C') {
@@ -17,6 +32,24 @@ const Calculator = () => {
     }
   }
 
+  const handleKeyPress = (event) => {
+    const key = event.key;
+    if (/^[0-9()\-+*/.^]$/.test(key)) {
+      handleButtonPress(key);
+    } else if (key === 'Enter') {
+      handleButtonPress('=');
+    } else if (key === 'Backspace') {
+      setInput(prevInput => prevInput.slice(0, -1));
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    }
+  })
+
   const buttons = [
     '(', ')', '√', '^',
     '7', '8', '9', '/',
@@ -26,18 +59,22 @@ const Calculator = () => {
     '=', 'C',
   ];
 
+  const keyboardButtons = () => {
+    return buttons.map(button => (
+      <button key={button} onClick={() => handleClick(button)}>{button}</button>
+    ));
+  }
+
   const renderButtons = () => {
     return buttons.map(button => (
-      <button key={button} onClick={() => handleClick(button)}>
-        {button}
-      </button>
+      <button key={button} onClick={() => handleClick(button)}>{button}</button>
     ));
   }
 
   return (
     <div className="calculator">
       <input className="display" type="text" value={input}/>
-      <div className="buttons">{renderButtons()}</div>
+      <div className="buttons">{renderButtons()} {keyboardButtons}</div>
     </div>
   );
 };
